9aad1342c2d7050275ca9607cce9f40a473afa6a
IguMavi <igor@mavigno.com>
2025-01-14 15:06:33 -0300

    Update jogo_da_forca.py
    add sistema de palavras aleatorias

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -1,13 +1,23 @@
-contador = 10
-palavra = "computador"
+import random
+
+caminho_arquivo = r'C:\Users\imavi\Desktop\Projects\Jogo da forca\banco.txt'
+
+with open(caminho_arquivo, 'r') as arquivo:
+    palavras = arquivo.read().splitlines()
+
+palavra = random.choice(palavras)
+
+contador = len(palavra)
 check = False
-sla = list("__________")
-quantidade = 10
+sla = ['_' for _ in range(len(palavra))]
+quantidade = len(palavra)
+
 while(check == False):
     tentativa = input()
     quantidade -= 1
     if(quantidade == 0):
         print("Acabou as tentativas")
+        print(f"A palavra era: {palavra}")
         break
     if(len(tentativa) == 1):
         for i in range(len(palavra)):
@@ -17,7 +27,7 @@ while(check == False):
         print(*sla)
     else:
         if(tentativa == palavra):
-            print(f"Acertou em {quantidade} tentativas!!")
+            print(f"Acertou em {len(palavra)-quantidade} tentativas!!")
             break
     print(f"Faltam {quantidade} tentativas")
     
