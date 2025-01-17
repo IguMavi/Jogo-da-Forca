8c64578bcbb193c36d38431aad5a528d790540b3
JoYoneyama <jvsyoneyama@gmail.com>
2025-01-17 20:02:46 -0300

    ...

--- app.py
+++ app.py
@@ -7,14 +7,24 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", palavra = palavra_secreta)
 
+@app.route("/get_palavra", methods=["GET"])
+def get_palavra():
+    tamanho = len(palavra_secreta)
+    return {"palavra": palavra_secreta, "tamanho": tamanho}
+
 @app.route("/verificar-tentativa", methods=["POST"])
 def verificacao():
     data = request.get_json()
     tentativa = data.get("tentativa")
-
-    if tentativa in palavra_secreta or tentativa == palavra_secreta:
-        return {"response": True}
-    return {"response": False}
+    
+    if tentativa in palavra_secreta:
+        acertos = []
+        for posicao in range(len(palavra_secreta)):
+            if tentativa == palavra_secreta[posicao]:   
+                acertos.append(posicao)
+        return {"posicao": acertos}
+    else:
+        return {"posicao": "errou"}
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,3 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    atualizarboard();
+});
+
+// --------------- //
+
 const input = document.getElementById("tentativa");
 
 document.getElementById("form-tentativa").addEventListener("submit", function (event) {
@@ -8,6 +14,34 @@ document.getElementById("form-tentativa").addEventListener("submit", function (e
 
 // ----------------- //
 
+function atualizarboard() {
+    const board = document.getElementById("board");
+    board.innerHTML = ""; // para limpar o conteudo e atualizar o jogo
+    
+    fetchPalavra().then((palavra) => {
+        let palavra_secreta = new Array(palavra.length).fill("_");
+
+        palavra_secreta.forEach(letra => {
+            let letraConteudo = document.createElement("h1");
+            letraConteudo.textContent = letra;
+            board.appendChild(letraConteudo);
+        });
+    });
+};
+
+
+// funcao q recebe a palavra gerada
+const fetchPalavra = () => {
+    fetch("http://127.0.0.1:5000/get_palavra", {
+        method: "GET",
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        dados = data;
+    })
+};
+ 
+// funcao q comunica com o back para verificar a letra
 async function fetchData(letra){
     const response = await fetch("http://127.0.0.1:5000/verificar-tentativa", {
         method: "POST",     // tipo de requisição
@@ -22,13 +56,22 @@ async function fetchData(letra){
     return await response.json()
 }
 
+// funcao q ativa a outra e muda o front
 async function verificar_tentativa() {
     var letra = document.getElementById("tentativa").value;
     const response = await fetchData(letra);
 
-    if (response.response === true) {
-        console.log("acertou")
-    } else {
-        console.log("errou")
-    }
-}   
+    fetchData().then((palavra) => {
+        let palavra_secreta = new Array(palavra.length).fill("_");
+
+        if (response.posicao === "errou") {
+            console.log("sla ne")
+        }else if (response.posicao) {
+            response.posicao.forEach(pos => {
+                palavra_secreta[pos] = letra
+            });
+        };
+    });
+
+    atualizarboard();
+};   
